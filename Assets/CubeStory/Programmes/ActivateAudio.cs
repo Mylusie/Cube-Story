5c2aec6b16fd6dcd3ff928d1e8552ce5dd6e41a7
Mylusie <aurore3356@gmail.com>
2024-03-23 14:08:13 -0400

    Terminé manque Audio et Papier

---
+++ Assets/CubeStory/Programmes/ActivateAudio.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class LancementAudio : MonoBehaviour
+{
+    public Transform cameraTransform; // Référence à la transform de la caméra
+    public GameObject cylinder; // Le cylindre qui définit les limites
+    public GameObject gameObjectToActivate; // Référence au GameObject à activer
+    
+    void Update()
+    {
+        // Détecter la sortie du cylindre
+        if (!IsCameraOutsideCylinder())
+        {
+            Debug.Log(IsCameraOutsideCylinder());
+            
+            // Désactiver le composant XRCharacterController du GameObject XR Origin
+            AudioSource characterController = GetComponent<AudioSource>();
+            if (characterController != null)
+            {
+                characterController.enabled = false;
+            }
+            else
+            {
+                Debug.LogError("XRCharacterController component not found on XR Origin GameObject.");
+            }
+            gameObjectToActivate.SetActive(false);
+        }
+    }
+
+    bool IsCameraOutsideCylinder()
+    {
+        // Obtenir le rayon du cylindre (supposant qu'il est aligné avec l'axe Y)
+        float cylinderRadius = cylinder.transform.localScale.x / 2f;
+
+
+        // Obtenir la position de la caméra sur le plan XZ (ignorer la hauteur Y)
+        Vector3 cameraPositionXZ = new Vector3(cameraTransform.position.x, 0, cameraTransform.position.z);
+
+        // Obtenir la position du centre du cylindre sur le plan XZ (ignorer la hauteur Y)
+        Vector3 cylinderCenterXZ = new Vector3(cylinder.transform.position.x, 0, cylinder.transform.position.z);
+
+        // Calculer la distance entre la caméra et le centre du cylindre sur le plan XZ
+        float distanceToCylinderCenter = Vector3.Distance(cameraPositionXZ, cylinderCenterXZ);
+
+        // La caméra est à l'extérieur du cylindre si la distance à son centre est supérieure à son rayon
+        bool isOutside = distanceToCylinderCenter > cylinderRadius;
+
+        Debug.Log(distanceToCylinderCenter + "Distance avec le centre du cyclindre");
+        Debug.Log(cylinderRadius + "rayon");
+
+        return isOutside;
+    }
+}
+
---
+++ Assets/CubeStory/Programmes/AudioManager M1.cs
@@ -0,0 +1,139 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class AudioManagerM1: MonoBehaviour
+{
+    public AudioSource[] audioSources; // Tableau des sources audio
+    public AudioClip[] audioClips; // Tableau des clips audio
+    public List<int> knightAudioIndices; // Indices des clips audio du chevalier
+    public List<int> fatherAudioIndices; // Indices des clips audio du père
+    public Material knightMaterial; // Matériau pour le chevalier
+    public Material fatherMaterial; // Matériau pour le père
+    public Material originalKnightMaterial; // Matériau d'origine pour le chevalier
+    public Material originalFatherMaterial; // Matériau d'origine pour le père
+    public Animator Anim; // GameObject de la bourse
+    public GameObject Cyl;
+
+    private Dictionary<int, bool> knightAudioPlaying; // Dictionnaire pour suivre les clips audio du chevalier en cours de lecture
+    private Dictionary<int, bool> fatherAudioPlaying; // Dictionnaire pour suivre les clips audio du père en cours de lecture
+    private bool uneFois = true; // Permet d'avoir la bourse qu'une seule fois de fait
+
+    private float timer = 0f;
+    private bool timerActive = false;
+
+    void Start()
+    {
+        // Initialiser les dictionnaires
+        knightAudioPlaying = new Dictionary<int, bool>();
+        fatherAudioPlaying = new Dictionary<int, bool>();
+
+        for (int i = 0; i < audioSources.Length && i < audioClips.Length; i++)
+        {
+            // Ajouter les clips audio du chevalier et du père aux dictionnaires
+            if (knightAudioIndices.Contains(i))
+            {
+                knightAudioPlaying.Add(i, true);
+            }
+
+            if (fatherAudioIndices.Contains(i))
+            {
+                fatherAudioPlaying.Add(i, true);
+            }
+        }
+
+        // Commence par charger la scène
+        StartCoroutine(PlayAudioClipsWithDelay());
+    }
+
+    IEnumerator PlayAudioClipsWithDelay()
+    {
+        // Jouer les clips audio avec un délai de 2 secondes entre chaque
+        for (int i = 0; i < audioSources.Length && i < audioClips.Length; i++)
+        {
+            // Vérifier si la source audio et le clip audio existent
+            if (audioSources[i] != null && audioClips[i] != null)
+            {
+                // Réglez le volume de l'AudioSource à 0.5
+                audioSources[i].volume = 0.85f;
+
+                // Assigner le clip audio à la source audio
+                audioSources[i].clip = audioClips[i];
+                // Jouer le son
+                audioSources[i].Play();
+
+                // Attendre que le clip audio en cours soit terminé
+                while (audioSources[i].isPlaying)
+                {
+                    // Mettre à jour les matériaux des personnages
+                    UpdateCharacterMaterials(i);
+
+                    yield return null;
+                }
+                 // Vérifier si c'est le dernier clip audio et qu'il est terminé
+                if (i == audioSources.Length - 1 && !audioSources[i].isPlaying)
+                    {
+                        
+                        // Charger une nouvelle scène et décharger la scène actuelle
+                        Anim.SetBool("Finit audio", true);
+
+                        timerActive = true;
+                    }
+
+                if (timerActive)
+                {
+                    timer += Time.deltaTime;
+
+                    if (timer >= 5f)
+                    {
+                        timerActive = false;
+
+                        // Afficher le GameObject
+                        Cyl.SetActive(true);
+                    }
+                }
+            }
+
+            // Attendre 2 secondes avant de passer au clip suivant
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    void UpdateCharacterMaterials(int index)
+    {
+        // Mettre à jour les matériaux des personnages en fonction de l'index du clip audio en cours
+        if (knightAudioPlaying.ContainsKey(index) && knightAudioPlaying[index])
+        {
+            // Mettre à jour le matériau du chevalier avec le matériau spécifié
+            SetCharacterMaterial(GameObject.FindWithTag("Knight").GetComponent<Renderer>(), knightMaterial);
+        }
+        else
+        {
+            // Mettre à jour le matériau du chevalier avec le matériau d'origine
+            SetCharacterMaterial(GameObject.FindWithTag("Knight").GetComponent<Renderer>(), originalKnightMaterial);
+        }
+
+        if (fatherAudioPlaying.ContainsKey(index) && fatherAudioPlaying[index])
+        {
+            // Mettre à jour le matériau du père avec le matériau spécifié
+            SetCharacterMaterial(GameObject.FindWithTag("Father").GetComponent<Renderer>(), fatherMaterial);
+        }
+        else
+        {
+            // Mettre à jour le matériau du père avec le matériau d'origine
+            SetCharacterMaterial(GameObject.FindWithTag("Father").GetComponent<Renderer>(), originalFatherMaterial);
+        }
+    }
+
+    void SetCharacterMaterial(Renderer characterRenderer, Material newMaterial)
+    {
+        // Mettre à jour le matériau du personnage
+        if (characterRenderer != null && newMaterial != null)
+        {
+            characterRenderer.material = newMaterial;
+        }
+    }
+
+    
+}
