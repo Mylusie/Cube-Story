fb398c8d7d0d3b5f9ac08729a485bcba260eacea
Lucas Lefauconnier <156128329+lucaslefau@users.noreply.github.com>
2024-03-26 11:11:10 -0400

    Maj manoir

--- Assets/CubeStory/Programmes/MouvementSortie1.cs
+++ Assets/CubeStory/Programmes/MouvementSortie1.cs
@@ -7,29 +7,32 @@ public class DeplacementCameraSortie1 : MonoBehaviour
     public GameObject porteSortie; // GameObject dont l'état d'activation contrôle le déplacement de la caméra
     public float seuilDistance = 0.01f;// Seuil de distance pour considérer que la caméra est arrivée à la nouvelle position
     public GameObject MainCamera;
+    private bool unite = false;
+
     //private bool enDeplacement = false; // Indique si la caméra est en cours de déplacement
 
     void Update()
     {
         // Vérifier si le GameObject de condition est actif et si la caméra n'est pas déjà en déplacement
-        if (porteSortie != null && porteSortie.activeSelf)//&& !enDeplacement
+        if (porteSortie != null && porteSortie.activeSelf && unite==false)//&& !enDeplacement
         {
             // Déplacer progressivement la caméra vers la nouvelle position
             Debug.Log("la porte est détectée");
-
-            //CapsuleCollider capsuleCollider = GetComponent<CapsuleCollider>();
+            
+            CapsuleCollider capsuleCollider = GetComponent<CapsuleCollider>();
             //capsuleCollider.enabled = false;
 
             //transform.position = Vector3.MoveTowards(transform.position, cible.transform.position, vitesseDeplacement * Time.deltaTime);
             //Debug.Log("Position : " + transform.position);
 
             ActivateAnimator(MainCamera);
-
+            unite = true;
 
             // Vérifier si la caméra a atteint la nouvelle position
+            Debug.Log(Vector3.Distance(transform.position, cible.transform.position));
             if (Vector3.Distance(transform.position, cible.transform.position) < seuilDistance)
             {
-                //capsuleCollider.enabled = true;
+                capsuleCollider.enabled = true;
                 Debug.Log("La caméra est arrivée à la nouvelle position.");
                 //enDeplacement = true;
                 porteSortie.SetActive(false);
@@ -45,12 +48,18 @@ public class DeplacementCameraSortie1 : MonoBehaviour
         //cible = targetGameObject;
     //}
     void ActivateAnimator(GameObject gameObject)
+        
     {
+        Debug.Log("3 ");
         if (gameObject != null)
         {
             Animator animator = gameObject.GetComponent<Animator>();
+            Debug.Log("1 ");
             if (animator != null)
+
             {
+
+                Debug.Log("2"); 
                 animator.enabled = true;
             }
         }
